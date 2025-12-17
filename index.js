d19ffeaad4f4713a06c688e4470d84763b7b78c1
Ralica Popova <ralicaterzieva90@gmail.com>
2025-12-17 16:24:02 +0200

    Fetch product and categories

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import data from './initial-data.js';
 
+const {products, categories} = data;
  //GraphQL schema - shape of your data and the operations clients can perform
 const typeDefs = `#graphql
   type Query {
-    hello: String
+    hello: [String!]!
+    products: [Product!]!
+    product(id: ID!): Product!
+    categories: [Category!]!
+  }
+
+  type Category {
+    id: ID!
+    name: String!
+  }
+
+  type Product {
+    name: String!
+    description: String!
+    quantity: Int
+    image: String
+    price: Float!
+    onSale: Boolean!
   }
 `;
 
 // Resolvers are functions that implement the logic behind the schema. They tell Apollo Server how to fetch or modify the data for each field defined in your typeDefs.
 const resolvers = {
   Query: {
-    hello: () => "Hello GraphQL 🚀",
+    hello: () => ["Hello GraphQL 🚀", "We are ready"],
+    products: () => {
+        return products;
+    },
+    product: (_, args) => {
+      const { id } = args;
+      return products.find(product => product.id === id);
+    },
+    categories: () => {
+        return categories;
+    }
   },
 };
 
